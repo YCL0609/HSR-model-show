1d23812762f027c55ccc649466dcfa5b20addcd5
YCL0609 <YCL20060609@163.com>
2025-11-26 22:15:13 -0400

    添加cfpage构建时日志输出

--- cfbuild.sh
+++ cfbuild.sh
@@ -2,11 +2,14 @@
 
 # 脚本路径
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo 脚本运行路径: $SCRIPT_DIR
 
 # Beta版本判断
 IsBeta=false
+echo Beta版本: $IsBeta
 
 # 百度统计
+echo 添加百度统计js代码...
 cat << EOF > "$SCRIPT_DIR/js/baidu.js"
 var _hmt = _hmt || [];
 (function () {
@@ -19,19 +22,23 @@ var _hmt = _hmt || [];
 EOF
 
 # 需要404.html来正常返回Http 404状态码
+echo 新建404.html...
 touch "$SCRIPT_DIR/404.html"
 
 # 删除非必要文件
+echo 删除非必要文件...
 rm -f "$SCRIPT_DIR\LICENSE"
 rm -f "$SCRIPT_DIR\README_EN.md"
 rm -f "$SCRIPT_DIR\README.md"
 
 # 切换到从网络加载公共函数库
+echo 切换到从网络加载公共函数库...
 sed -i 's/.\/outsite\/dist\/function.bundle.min.js/https:\/\/api.ycl.cool\/js\/function.bundle.min.js/' "$SCRIPT_DIR/index.html"
 sed -i 's/.\/outsite\/dist\/function.bundle.min.js/https:\/\/api.ycl.cool\/js\/function.bundle.min.js/' "$SCRIPT_DIR/3d.html"
 
 # 处理Beta版本
 if $IsBeta; then
+    echo 处理Beta版本提示信息...
     sed -i 's/<!-- \*当前为测试版 Is currently in beta -->/\*当前为测试版 Is currently in beta/' "$SCRIPT_DIR/index.html"
     sed -i 's/<!-- \*当前为测试版 Is currently in beta -->/\*当前为测试版 Is currently in beta/' "$SCRIPT_DIR/3d.html"
 fi
